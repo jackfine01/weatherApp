6fd86aaaf1117961e5d927da96e0f5083c08429a
jack <jackfine01@gmail.com>
2025-02-11 12:17:16 -0500

    adding a constructor function instead of an array for weatehr daata.

--- js.js
+++ js.js
@@ -1,52 +1,45 @@
 //Write the functions that hit the API. 
 // You’re going to want functions that can take a location and return the weather data for that location. 
 // For now, just console.log() the information.
-
+function weatherData(temp, conditions,datetime,feels,humidity,precip,precipprob){
+    this.temp = temp,
+    this.conditions = conditions,
+    this.datetime = datetime,
+    this.feels = feels
+    this.humidity = humidity
+    this.precip = precip
+    this.precipprob = precipprob
+}
 function callWeatherData(city){
     fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+city+'?key=2TC8FT4VR7JK57EJFDU2H92Z9', {mode:'cors'})
     .then(function(response) {
         return response.json();
     })
     .then(function(response){
-        const weatherData = [];
         console.log(response);
-
         let temp = response.currentConditions.temp
-        console.log(temp);
-        weatherData.push(temp);
-
+        // console.log(temp);
         let conditions = response.currentConditions.conditions;
-        console.log(conditions);
-        weatherData.push(conditions);
-
+        // console.log(conditions);
         let datetime = response.currentConditions.datetime
-        console.log(datetime);
-        weatherData.push(datetime);
-
+        // console.log(datetime);
         let feels = response.currentConditions.feelslike
-        console.log(feels);
-        weatherData.push(feels);
-
+        // console.log(feels);
         let humidity = response.currentConditions.humidity
-        console.log(humidity);
-        weatherData.push(humidity);
-
+        // console.log(humidity);
         let precip = response.currentConditions.precip
-        console.log(precip);
-        weatherData.push(precip);
-
+        // console.log(precip);
         let precipprob = response.currentConditions.precipprob
-        console.log(precipprob);
-        weatherData.push(precipprob);
-
-        console.log(weatherData)
-        return weatherData;
+        // console.log(precipprob);
+        const data = new weatherData(temp, conditions, datetime, feels,humidity,precip,precipprob)
+        return data;
     })
 };
 
 let search = 'washington';
-let weatherData = callWeatherData(search);
-console.log(weatherData[0])
+let weather = callWeatherData(search);
+console.log(weather)
+
 const searchInput = document.getElementById('searchBar')
 searchInput.addEventListener('input', () => {
     search = searchInput.value;
