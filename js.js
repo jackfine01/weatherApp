a07e6a17b54c0a01373bac91bc10002b123c58fc
jack <jackfine01@gmail.com>
2025-02-11 13:17:20 -0500

    finsishing mechanchally what the phoject needs to do.

--- js.js
+++ js.js
@@ -11,34 +11,56 @@ function weatherData(temp, conditions,datetime,feels,humidity,precip,precipprob)
     this.precipprob = precipprob
 }
 function callWeatherData(city){
-    fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+city+'?key=2TC8FT4VR7JK57EJFDU2H92Z9', {mode:'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response){
-        console.log(response);
-        let temp = response.currentConditions.temp
-        // console.log(temp);
-        let conditions = response.currentConditions.conditions;
-        // console.log(conditions);
-        let datetime = response.currentConditions.datetime
-        // console.log(datetime);
-        let feels = response.currentConditions.feelslike
-        // console.log(feels);
-        let humidity = response.currentConditions.humidity
-        // console.log(humidity);
-        let precip = response.currentConditions.precip
-        // console.log(precip);
-        let precipprob = response.currentConditions.precipprob
-        // console.log(precipprob);
-        const data = new weatherData(temp, conditions, datetime, feels,humidity,precip,precipprob)
-        return data;
-    })
+    try{
+        fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+city+'?key=2TC8FT4VR7JK57EJFDU2H92Z9'
+        , {mode:'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response){
+            console.log(response);
+            let temp = response.currentConditions.temp
+            let conditions = response.currentConditions.conditions;
+            let datetime = response.currentConditions.datetime
+            let feels = response.currentConditions.feelslike
+            let humidity = response.currentConditions.humidity
+            let precip = response.currentConditions.precip
+            let precipprob = response.currentConditions.precipprob
+            const data = new weatherData(temp, conditions, datetime, feels,humidity,precip,precipprob)
+            console.log(data);
+            weather = data;
+            clearAll();
+            createCard(weather.temp)
+            createCard(weather.conditions)
+            createCard(weather.datetime)
+            createCard(weather.humidity)
+            createCard(weather.precip)
+            createCard(weather.precipprob)
+        })
+    }
+    catch(er){
+        console.log(er)
+    }
 };
 
+function clearAll(){
+    let cards = document.querySelectorAll('.card')
+    cards.forEach(card => 
+        card.remove()
+    )
+}
+function createCard(text){
+    const main = document.getElementById('main');
+    const card = document.createElement('div');
+    card.textContent = text;
+    card.className = 'card';
+    main.appendChild(card);
+}
+
 let search = 'washington';
-let weather = callWeatherData(search);
-console.log(weather)
+let weather = new weatherData(callWeatherData(search))
+
+
 
 const searchInput = document.getElementById('searchBar')
 searchInput.addEventListener('input', () => {
